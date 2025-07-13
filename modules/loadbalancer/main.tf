7b92f712402b424d89c0e467e4e57fcea1b9ad38
narendiransankar <narendiransankar1699@gmail.com>
2025-07-13 18:06:22 +0530

    Update main.tf

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -108,7 +108,7 @@ resource "aws_security_group" "alb_sg" {
   ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
