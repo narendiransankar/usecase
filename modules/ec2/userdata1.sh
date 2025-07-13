05696af1ce84028e4405d8513976bbb3d909f053
narendiransankar <narendiransankar1699@gmail.com>
2025-07-13 17:02:13 +0530

    Update userdata1.sh

--- modules/ec2/userdata1.sh
+++ modules/ec2/userdata1.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+apt update -y
+apt install -y apache2
+systemctl start apache2
+systemctl enable apache2
+mkdir -p /var/www/html/images
+echo "<h1>Welcome to Images</h1>" > /var/www/html/images/index.html
+sudo ln -s /var/www/html/images/index.html /var/www/html/index.html             
 
