f9c30b4b32273552ec1ab239dd6af0519b05546f
narendiransankar <narendiransankar1699@gmail.com>
2025-07-13 17:27:38 +0530

    Update main.tf

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -106,15 +106,9 @@ resource "aws_security_group" "alb_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -125,7 +119,7 @@ resource "aws_security_group" "alb_sg" {
 #     security_groups = [var.alb_security_group_id]
 #   }
 
-  # Use the default egress rule (allow all outbound traffic)
+
   egress {
     from_port   = 0
     to_port     = 0
