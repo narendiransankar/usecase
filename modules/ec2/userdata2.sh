e0c5da82ffb5b114003cf4ab91720bdc3c99d3a5
narendiransankar <narendiransankar1699@gmail.com>
2025-07-13 17:03:50 +0530

    Update userdata2.sh

--- modules/ec2/userdata2.sh
+++ modules/ec2/userdata2.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+apt update -y
+apt install -y apache2
+systemctl start apache2
+systemctl enable apache2
+mkdir -p /var/www/html/register
+echo "<h1>Welcome to Register</h1>" > /var/www/html/register/index.html
+sudo ln -s /var/www/html/register/index.html /var/www/html/index.html
