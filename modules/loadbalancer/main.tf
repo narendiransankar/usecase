7cdc212ba1b23fba71331fc36355126603ec0d83
narendiransankar <narendiransankar1699@gmail.com>
2025-07-13 17:25:14 +0530

    Update main.tf

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -16,15 +16,8 @@ resource "aws_lb_target_group" "tg_homepage" {
   protocol    = "HTTP"
   target_type = "instance"
   vpc_id      = var.vpc_id
-
-  health_check {
-    path                = "/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
 }
+
 resource "aws_lb_target_group_attachment" "tg_homepage_attachment" {
   target_group_arn = aws_lb_target_group.tg_homepage.arn
   target_id        = var.instance_a_id
@@ -38,15 +31,8 @@ resource "aws_lb_target_group" "tg_images" {
   protocol    = "HTTP"
   target_type = "instance"
   vpc_id      = var.vpc_id
-
-  health_check {
-    path                = "/images/index.html"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
 }
+
 
 resource "aws_lb_target_group_attachment" "tg_images_attachment" {
   target_group_arn = aws_lb_target_group.tg_images.arn
@@ -64,15 +50,8 @@ resource "aws_lb_target_group" "tg_register" {
   protocol    = "HTTP"
   target_type = "instance"
   vpc_id      = var.vpc_id
-
-  health_check {
-    path                = "/register/index.html"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
 }
+
 resource "aws_lb_target_group_attachment" "tg_register_attachment" {
   target_group_arn = aws_lb_target_group.tg_register.arn
   target_id        = var.instance_c_id
@@ -96,7 +75,7 @@ resource "aws_lb_listener_rule" "rule_images" {
 
   condition {
     path_pattern {
-      values = ["/images/*"]
+      values = ["/images*"]
     }
   }
 
@@ -111,7 +90,7 @@ resource "aws_lb_listener_rule" "rule_register" {
 
   condition {
     path_pattern {
-      values = ["/register/*"]
+      values = ["/register*"]
     }
   }
 
